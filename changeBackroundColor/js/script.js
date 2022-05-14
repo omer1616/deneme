884b2161e44d380d29458c3b61985bf90a1c3dbd
omer1616 <omerinci415@gmail.com>
2022-05-14 22:42:46 +0300

    bos

---
+++ changeBackroundColor/js/script.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('button')
+const body = document.querySelector('body')
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+
+
+
+
+body.style.backgroundColor = "violet"
+button.addEventListener('click', changeBackgroundColor)
+
+function changeBackgroundColor() {
+    const colorIndex = parseInt(Math.random()*colors.length)
+    body.style.backgroundColor = colors[colorIndex]
+}
