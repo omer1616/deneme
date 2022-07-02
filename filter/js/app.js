990ee511cb53cbb4dbf684af32a892b2cb25bc79
omer1616 <omerinci415@gmail.com>
2022-07-02 21:17:11 +0300

    editing filter

--- filter/js/app.js
+++ filter/js/app.js
@@ -1,24 +1,98 @@
-(function () {
-  //Select the input element
-  const form = document.querySelector("#message-form");
-  //Set up Submit Button
-  form.addEventListener("submit", function (e) {
-    // prevent the form's default submission action
-    e.preventDefault();
-    //Get user's input from from
-    const message = document.querySelector("#message");
-    const feedback = document.querySelector(".feedback");
-    const messageContent = document.querySelector(".message-content");
+// Work to get the filter buttons working
+(function(){
+    // const buttons = document.querySelectorAll('.btn')
+    // const storeItems = document.querySelectorAll('.store-item')
+    //console.log(buttons)
+    // buttons.forEach(function(button){
+    //     button.addEventListener('click', function(e){
+    //         //prevent the default link jump to top of page
+    //         e.preventDefault()
+    //         //grab the filter button that was clicked
+    //         const filter = e.target.dataset.filter
+    //         if (filter === 'all'){
+    //             //show all items
+    //             storeItems.forEach(function(item){
+    //                 item.style.display = 'block'
+    //             })
+    //         } else if (filter === 'cakes'){
+    //             storeItems.forEach(function(item){
+    //                 if (item.classList.contains('cakes')){
+    //                     item.style.display = 'block'
+    //                 } else {
+    //                     item.style.display = 'none'
+    //                 }
+    //             })
+    //         } else if (filter === 'cupcakes'){
+    //             storeItems.forEach(function(item){
+    //                 if (item.classList.contains('cupcakes')){
+    //                     item.style.display = 'block'
+    //                 } else {
+    //                     item.style.display = 'none'
+    //                 }
+    //             })
+    //         } else if (filter === 'sweets'){
+    //             storeItems.forEach(function(item){
+    //                 if (item.classList.contains('sweets')){
+    //                     item.style.display = 'block'
+    //                 } else {
+    //                     item.style.display = 'none'
+    //                 }
+    //             })
+    //         } else if (filter === 'doughnuts'){
+    //             storeItems.forEach(function(item){
+    //                 if (item.classList.contains('doughnuts')){
+    //                     item.style.display = 'block'
+    //                 } else {
+    //                     item.style.display = 'none'
+    //                 }
+    //             })
+    //         }
+    //     })
+    // })
 
-    if (message.value === "") {
-      feedback.classList.add("show");
-      setTimeout(() => {
-        feedback.classList.remove("show");
-      }, 1000);
-    } else {
-     messageContent.textContent =  message.value;
-     message.value =  "";
-     
-    }
-  });
+    // refactor to get rid of DRY code
+    const buttons = document.querySelectorAll('.btn')
+    const storeItems = document.querySelectorAll('.store-item')
+
+    buttons.forEach((button)=> {
+        button.addEventListener('click', (e) => {
+            e.preventDefault()
+            const filter = e.target.dataset.filter
+            
+            storeItems.forEach((item)=> {
+                if (filter === 'all'){
+                    item.style.display = 'block'
+                } else {
+                    if (item.classList.contains(filter)){
+                        item.style.display = 'block'
+                    } else {
+                        item.style.display = 'none'
+                    }
+                }
+            })
+        })
+    })
+
+})();
+
+//wire up filter search box
+(function(){
+
+    const searchBox = document.querySelector('#search-item')
+    const storeItems = document.querySelectorAll('.store-item')
+
+    searchBox.addEventListener('keyup', (e) => {
+    
+        const searchFilter = e.target.value.toLowerCase().trim()
+        //display only items that contain filter input
+
+        storeItems.forEach((item) => {
+            if (item.textContent.includes(searchFilter)){
+                item.style.display = 'block'
+            } else {
+                item.style.display = 'none'
+            }
+        })
+    })
+
 })();
